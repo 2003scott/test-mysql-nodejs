7e233e5a8ec04feb8c253baad5ffdd148293824b
Ckott <diegoangeles130703@gmail.com>
2023-08-22 04:55:40 -0500

    crud categorias rutas express

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -6,7 +6,7 @@ export const getCategorias = async (req,res) => {
         res.json(rows)
     }catch(error){
         return res.status(500).json({
-            message : 'Algo anda mal'
+            message : 'Algo fallo al pedido de categorias'
         })
     }
 }
@@ -15,12 +15,12 @@ export const getCategoriasid = async (req,res) => {
     try{
         const [rows] = await pool.query('select * from categoria where codcat= ?',[req.params.id])
         if(rows.length <=0) return res.status(404).json({
-            message : "Categorias not found"
+            message : "Categorias : no hay inserciones o se encuentra resultados"
         })
         res.json(rows[0])   
     } catch(error){
         return res.status(500).json({
-            message : "Algo no va bien"
+            message : "Algo fallo al pedido de categoria id"
         })
     }
 }
@@ -36,7 +36,45 @@ export const createCategorias = async (req,res) => {
         })
     } catch (error) {
         return res.status(500).json({
-            message : 'Algo salio mal'
+            message : 'Algo fallo al crear una categoria'
         })
     }
 }
+
+export const updateCategorias = async (req,res) => {
+    const  {codcat} = req.params
+    const {nomcat,estacat} = req.body
+    try {
+        const [result] = await pool.query('update categoria set nomcat = ifnull(?,nomcat), estacat = ifnull(?,estacat) where codcat = ?',[nomcat,estacat,codcat])
+        console.log(result)
+        
+        if(result.affectedRows === 0) return res.status(404).json({
+            message : 'Categoria a fallado al hacer update'
+        })
+
+        const [rows] = await pool.query('select * from categoria where codcat = ?',[codcat])
+        res.json(rows[0])
+        
+    } catch (error) {
+        return res.status(500).json({
+            message : "Categoria - a pasado un error"
+        })
+    }
+
+
+}
+
+export const deleteCategorias = async (req,res) => {
+    try {
+        const [result] = await pool.query('delete from categoria where codcat= ?', [req.params.id])
+        if (result.affectedRows <=0) return res.status(404).json({
+            message: 'Categorias : no hay ningun resultado'
+         })
+        res.sedStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message : 'Algo fallo al elimnar una categoria'
+        })
+    }
+
+} 
--- src/routes/categoria.routes.js
+++ src/routes/categoria.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getCategorias,getCategoriasid,createCategorias } from "../controllers/categoria.controller.js";
+import { getCategorias,getCategoriasid,createCategorias,deleteCategorias,updateCategorias } from "../controllers/categoria.controller.js";
 
 const router = Router()
 
 router.get('/categorias',getCategorias)
 router.get('/categorias/:id', getCategoriasid)
 router.post('/categorias', createCategorias)
+router.patch('/categorias/:id', updateCategorias)
+router.delete('/categorias/:id', deleteCategorias)
 
 export default router
 
